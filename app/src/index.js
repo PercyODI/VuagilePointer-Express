eb2afe69d71c61f2ebed3c4a8bd5fe24fcdf3ad3
Pearse Hutson <pearse.2008@gmail.com>
2020-03-22 15:10:25 -0500

    Allow CSS loading into separate dist files.

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 ///@ts-check
 import $ from "jquery"
+import "./styles.css"
 
 $(function(){
     alert("It worked! or did it?");
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,17 +1,29 @@
+///@ts-check
+
 const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, '../app/src/index.js'),
+    output: {
+        filename: '[name].[contenthash].bundle.js',
+        path: path.resolve(__dirname, '../app/dist')
+    },
     plugins: [
         new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "../app/src/index.html")
         })
     ],
-    output: {
-        filename: '[name].[contenthash].bundle.js',
-        path: path.resolve(__dirname, '../app/dist')
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
     }
 }
