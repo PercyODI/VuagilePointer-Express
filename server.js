5b1b5c09a30bcb619672e8223692d3c7a0988f29
Pearse Hutson <pearse.2008@gmail.com>
2020-03-22 14:42:48 -0500

    Using environment variable for PORT.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use("/app", express.static("app/dist"));
 app.get("/", (req, res) => res.redirect("/app/index.html"))
