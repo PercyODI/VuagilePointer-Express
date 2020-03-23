688e1271a56adf16a5a50d7fe93731173574c7d1
Pearse Hutson <pearse.2008@gmail.com>
2020-03-22 20:35:05 -0500

    Added socket support with quick test

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,19 @@
 ///@ts-check
 import $ from "jquery"
+import io from "socket.io-client"
 import "./styles.css"
 
-$(function(){
-    alert("It worked! or did it?");
+const appSettings = {
+    hostUrl: process.env.NODE_ENV == "production" 
+        ? "https://vuagile-pointer.herokuapp.com" 
+        :"http://localhost:3000"
+}
+
+// $(function(){
+//     alert("It worked! or did it?");
+// })
+
+let socket = io.connect("http://localhost:3000");
+socket.on("news", function(data){
+    console.dir(data)
 })
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+///@ts-check
+
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
+// socket.io things
+var server = new (require("http").Server)(app);
+var io = require("socket.io")(server);
+
+server.listen(port, () => console.log(`Listening on port ${port}\nLink: http://localhost:${port}`))
+
+// Express Routes
 app.use("/app", express.static("app/dist"));
 app.get("/", (req, res) => res.redirect("/app/index.html"))
 
-app.listen(port, () => console.log(`Listening on port ${port}\nLink: http://localhost:${port}`))
+// app.listen(port, () => console.log(`Listening on port ${port}\nLink: http://localhost:${port}`))
+
+// socket.io Routes
+io.on("connection", function(socket) {
+    socket.emit("news", {hello: true})
+    console.log("Got a connection: ")
+    console.dir(socket)
+});
